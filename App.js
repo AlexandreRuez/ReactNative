500d6aa565780d1ef16ced4d9f12163b278070b6
AlexandreRuez <alexandre.ruez@icloud.com>
2019-07-27 17:34:31 +0200

    Start

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Search from './Components/Search'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Search/>
+    )
+  }
+}
 
 export default function App() {
   return (
