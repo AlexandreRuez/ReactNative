1ba1fa34b4a73813aacffaaca912f8c1323082bb
AlexandreRuez <alexandre.ruez@icloud.com>
2019-07-28 01:33:46 +0200

    Ajout de la fonction recherche

--- Components/Search.js
+++ Components/Search.js
@@ -1,19 +1,45 @@
 // Components/Search.js
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
-import films from '../Helpers/filmsData'
 import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            films: [],
+            searchedText: ""
+        }
+    }
+
+    _loadFilms() {
+        console.log(this.state.searchedText)
+        if (this.state.searchedText.length > 0) {
+            getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
+                this.setState({films: data.results})
+            });
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.setState({ searchedText: text })
+    }
+
     render() {
+        console.log("RENDER")
         return (
             <View style={[styles.main_container, { marginTop: 20}]}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button title='Rechercher' onPress={() => {}}/>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                />
+                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <FilmItem/>}
+                    renderItem={({item}) => <FilmItem film={item}/>}
                 />
             </View>
         )
