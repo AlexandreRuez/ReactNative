72bbffce2fb566e23bbc448e2d5e68e8c0d7bbb4
AlexandreRuez <alexandre.ruez@icloud.com>
2019-07-29 11:28:00 +0200

    Ajout de la fonction recherche

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -2,9 +2,13 @@
 
 const API_TOKEN = "cac285109e45514aaa23a1c4578f24ed";
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
+}
+
+export function getImageFromApi (name) {
+    return 'https://image.tmdb.org/t/p/w300' + name
 }
--- Components/Search.js
+++ Components/Search.js
@@ -1,46 +1,82 @@
 // Components/Search.js
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 import FilmItem from './FilmItem'
 import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
     constructor(props) {
         super(props)
+        this.searchedText = ""
+        this.page = 0
+        this.totalPages = 0
         this.state = {
             films: [],
-            searchedText: ""
+            isLoading: false
         }
     }
 
     _loadFilms() {
-        console.log(this.state.searchedText)
-        if (this.state.searchedText.length > 0) {
-            getFilmsFromApiWithSearchedText(this.state.searchedText).then(data => {
-                this.setState({films: data.results})
+        if (this.searchedText.length > 0) {
+            this.setState({ isLoading: true })
+            getFilmsFromApiWithSearchedText(this.searchedText, this.page+1).then(data => {
+                this.page = data.page
+                this.totalPages = data.total_pages
+                this.setState({
+                    films: [ ...this.state.films, ...data.results ],
+                    isLoading: false
+                })
             });
         }
     }
 
+    _searchFilms() {
+        this.page = 0
+        this.totalPages = 0
+        this.setState({
+            films: []
+        },() => {
+            this._loadFilms()
+        })
+    }
+
     _searchTextInputChanged(text) {
-        this.setState({ searchedText: text })
+        this.searchedText = text
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large' />
+                    {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+                </View>
+            )
+        }
     }
 
     render() {
-        console.log("RENDER")
         return (
             <View style={[styles.main_container, { marginTop: 20}]}>
                 <TextInput
                     style={styles.textinput}
                     placeholder='Titre du film'
                     onChangeText={(text) => this._searchTextInputChanged(text)}
+                    onSubmitEditing={() => this._searchFilms()}
                 />
-                <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+                <Button title='Rechercher' onPress={() => this._searchFilms()}/>
                 <FlatList
                     data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                        if (this.page < this.totalPages) { // On vérifie qu'on n'a pas atteint la fin de la pagination (totalPages) avant de charger plus d'éléments
+                            this._loadFilms()
+                        }
+                    }}
                 />
+                {this._displayLoading()}
             </View>
         )
     }
@@ -58,6 +94,15 @@ const styles = StyleSheet.create({
         borderColor: '#000000',
         borderWidth: 1,
         paddingLeft: 5
+    },
+    loading_container: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 100,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 export default Search
